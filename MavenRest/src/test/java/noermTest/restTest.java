1b63747b7a5d86cdaa40ec2a1dca3d9f54450804
Noerm177 <noerm177@gmail.com>
2019-06-10 10:12:21 -0500

    primer commit

---
+++ MavenRest/src/test/java/noermTest/restTest.java
@@ -0,0 +1,21 @@
+package noermTest;
+
+import org.testng.annotations.Test;
+
+public class restTest {
+
+	@Test
+	private void dataRest() 
+	{
+		System.out.println("DATA");
+	
+	}
+	
+	@Test
+	private void getRest() 
+	{
+		System.out.println("GET");
+
+
+	}
+}
---
+++ MavenRest/src/test/java/noermTest/selTest.java
@@ -0,0 +1,19 @@
+package noermTest;
+
+import org.testng.annotations.Test;
+
+public class selTest {
+
+	@Test
+	private void BrowserAUT() 
+	{
+		System.out.println("BrowserAUT");		
+	}
+	
+	@Test
+	private void elementUI() 
+	{
+		System.out.println("ELEMENTAUT");
+	}
+	
+}
